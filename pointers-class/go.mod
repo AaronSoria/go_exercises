84c892513667d9ce82ecea735abf096e71e7307e
AaronS28 <soria_aaron@outlook.com>
2025-07-11 11:34:42 -0300

    clase punteros - final

---
+++ pointers-class/go.mod
@@ -0,0 +1,3 @@
+module example.com/pointers-course
+
+go 1.24.3
---
+++ pointers-class/pointers.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 32
+	agePointer := &age
+	fmt.Println("Age: ", age)
+	getAdultYears(agePointer)
+	fmt.Println(age)
+}
+
+func getAdultYears(age *int) {
+	*age = *age - 18
+}
